200ac3a901760f2b322620dae3b66d017d335f56
Beder Danilo Casa Condori <108836354+BederCC@users.noreply.github.com>
2024-08-27 23:49:33 -0500

    Update consultas.js

--- consultas.js
+++ consultas.js
@@ -13,3 +13,40 @@ db.users.insertOne({
     },
     hobbies: []     // Lista de pasatiempos del usuario (arreglo de cadenas de texto)
 })
+
+
+// Este comando inserta varios documentos en la colección 'users' al mismo tiempo.
+// Es útil cuando se necesita añadir múltiples usuarios en una sola operación.
+
+db.users.insertMany([
+    {
+        name: "",       // Nombre del primer usuario (cadena de texto)
+        email: "",      // Correo electrónico del primer usuario (cadena de texto)
+        age: 0,         // Edad del primer usuario (número)
+        address: {      // Dirección del primer usuario (objeto anidado)
+            street: "", // Calle (cadena de texto)
+            city: "",   // Ciudad (cadena de texto)
+            state: "",  // Estado o provincia (cadena de texto)
+            zip: ""     // Código postal (cadena de texto)
+        },
+        hobbies: []     // Lista de pasatiempos del primer usuario (arreglo de cadenas de texto)
+    },
+    {
+        name: "",       // Nombre del segundo usuario (cadena de texto)
+        email: "",      // Correo electrónico del segundo usuario (cadena de texto)
+        age: 0,         // Edad del segundo usuario (número)
+        address: {      // Dirección del segundo usuario (objeto anidado)
+            street: "", // Calle (cadena de texto)
+            city: "",   // Ciudad (cadena de texto)
+            state: "",  // Estado o provincia (cadena de texto)
+            zip: ""     // Código postal (cadena de texto)
+        },
+        hobbies: []     // Lista de pasatiempos del segundo usuario (arreglo de cadenas de texto)
+    }
+    // Puedes añadir más documentos según sea necesario
+])
+
+// Este comando elimina una colección completa en la base de datos actual.
+// Se utiliza cuando ya no necesitas la colección y deseas eliminar todos sus documentos y el esquema asociado.
+// Ten en cuenta que esta operación es irreversible y eliminará permanentemente todos los datos en la colección.
+db.collectionName.drop()
