3d0a5c89fef15f3a3d5563aad371df1cc6250dbe
Beder Danilo Casa Condori <108836354+BederCC@users.noreply.github.com>
2024-08-28 00:03:23 -0500

    Update find.js

--- find.js
+++ find.js
@@ -7,3 +7,23 @@
 // este comando solo devolverá aquellos documentos donde el campo 'type' tenga el valor 'movie'.
 
 db.movies.find({type: 'movie'})
+
+
+
+// Este objeto de consulta se utiliza para buscar documentos en una colección de MongoDB que cumplan con dos condiciones:
+// 1. El campo 'type' debe ser igual a 'movie'.
+// 2. El campo 'released' debe ser una fecha igual o posterior al 1 de enero del 2000.
+
+// Se utiliza en conjunto con el método 'find()' para recuperar películas que se lanzaron a partir del año 2000.
+// La función '$gte' (greater than or equal) indica que solo se seleccionarán los documentos donde la fecha de lanzamiento
+// ('released') sea mayor o igual a la fecha especificada.
+
+// Ejemplo de uso en una consulta:
+// Si quieres encontrar todas las películas lanzadas desde el año 2000 en adelante, usarías la siguiente consulta:
+
+db.movies.find({
+    type: 'movie',             // Filtra solo los documentos donde 'type' es igual a 'movie'
+    released: {                // Aplica una condición al campo 'released'
+        $gte: ISODate('2000-01-01') // Selecciona fechas iguales o posteriores al 1 de enero del 2000
+    }
+})
